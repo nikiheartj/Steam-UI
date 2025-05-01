afb4c065e2288d3f653fb669f355c73d12f6bafb
Nikij <niki.heartj@gmail.com>
2025-05-01 17:14:41 +0300

    Enhance GitHub Actions workflow: add BaseURL input for tests and setup Java 17; update Playwright config to use environment variable for baseURL

--- playwright.config.js
+++ playwright.config.js
@@ -26,6 +26,7 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ["html", { open: "never" }],
+    ["line"],
     [
       "allure-playwright",
       {
@@ -41,7 +42,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: "https://store.steampowered.com/",
+    baseURL: process.env.BaseURL ?? "https://store.steampowered.com/",
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on",
     screenshot: "on",
